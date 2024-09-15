7f5ede9450f592270b819c60ae17915fcdb225ff
pppls <pppls_@outlook.com>
2024-09-15 11:18:09 +0200

    Eerste opzet van backend - nog niet gelinkt aan frontend

---
+++ Backend/GridHub.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace WebApplication1;
+
+public class GridHub : Hub
+{
+    public async Task SendMessage(string user, string message)
+    {
+        await Clients.All.SendAsync("UpdateGridElement", user, message);
+    }
+}
---
+++ Backend/Program.cs
@@ -0,0 +1,24 @@
+using WebApplication1;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddSignalR();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+app.MapHub<GridHub>("/gridHub");
+app.Run();
