6161b458916220d71b785a241bc55e3d1e656294
pppls <pppls_@outlook.com>
2024-09-18 20:15:03 +0200

    add optimisticconcurrency token

---
+++ Backend/Migrations/20240917082242_AddConcurrencyToken.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication1.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddConcurrencyToken : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "Timestamp",
+                table: "GridElements",
+                type: "BLOB",
+                rowVersion: true,
+                nullable: false,
+                defaultValue: new byte[0]);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Timestamp",
+                table: "GridElements");
+        }
+    }
+}
